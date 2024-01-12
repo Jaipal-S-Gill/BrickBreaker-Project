477b9448bdf66c0d5e4dbff09b915d5c4a779f0f
Jaipal-S-Gill <124804130+Jaipal-S-Gill@users.noreply.github.com>
2024-01-12 13:28:19 -0700

    Add files via upload

---
+++ Brickbreaker/main.js
@@ -0,0 +1,51 @@
+//board
+let boardWidth = 500;
+let boardHeight = 500; 
+let context; 
+
+//player
+let playerWidth = 80;
+let playerHeight = 10; 
+let playerVelocityX = 10;
+
+
+let player = {
+    x : boardWidth/2 - playerWidth/2,
+    y: boardHeight - playerHeight - 5, 
+    width : playerWidth, 
+    height : playerHeight, 
+    velocityX : playerVelocityX
+}
+
+window.onload = function() {
+    board = document.getElementById("board"); 
+    board.height = boardHeight; 
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    //draw initial player
+    context.fillStyle = "skyblue";
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", movePlayer)
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+    //player
+    context.fillStyle = "skyblue";
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+}
+
+function movePlayer(e) {
+
+    if(e.code == "ArrowLeft") {
+        player.x -= player.velocityX; 
+    }
+    else if (e.code == "ArrowRight") {
+        player.x += player.velocityX; 
+    }
+}
